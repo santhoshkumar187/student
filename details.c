e1892e4cc1af889a9cae7c3ef7b97539571b6d23
santhoshkumar187 <santhoshkumars.20cse@kongu.edu>
2021-10-23 11:00:45 +0530

    first commit

---
+++ details.c
@@ -0,0 +1,24 @@
+#include <stdio.>
+void main()
+{
+	char name[] = "santhoshkumar";
+	char roll[] = "20CSR187";
+	char fathername[] = "kumar";
+	char mothername[] = "geetha";
+	int age = 18;
+	int mobnum = 8767;
+	char email[] = "san@ko";
+	char bloodgrp[] = "o+";
+	char add[] = "salem";
+	int aadhar[] = 43345545;
+	printf("%s",name);
+	printf("%s",roll);
+	printf("%s",fathername);
+	printf("%s",mothername);
+	printf("%d"age);
+	printf("%d",mobnum);
+	printf("%s",email);
+	printf("%s",bloodgrp);
+	printf("%s",add);
+	printf("%d",aadhar);
+}
