c31273ff1ef4b7e2a9133470ad04dda6b384c338
santhoshkumar187 <santhoshkumars.20cse@kongu.edu>
2021-10-23 11:19:46 +0530

    incurance commit

---
+++ insdetail.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int age,pnum,anum;
+	char name[] , Nname[] , Rname[], occu[] , email[];
+
+	printf("enter the name");
+	scanf("%s",name);
+	printf("enter the nominee name:");
+	scanf("%s",Nname);
+	printf("enter the relationship of nominee:");
+	scanf("%s",Rname);
+	printf("enter the nomine age:");
+	scanf("%d",&age);
+	printf("enter the nominee phone num:");
+	scanf("%d",&pnum);
+	printf("enter the nominee oucupation:");
+	scanf("%s",occu);
+	printf("enter the nominee email:");
+	scanf("%s",email);
+	printf("enter the nominee aadhar num:");
+	scanf("%d",&anum);
+	
+}
