4ebb9ab72b7635b1bab18ffc21a9f66210c97a38
santhoshkumar187 <santhoshkumars.20cse@kongu.edu>
2021-10-25 19:37:12 +0530

    npetel commit

---
+++ coursedetail.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int sem , one , vadd , npetel;
+        char rolln[] , name[] , nval[];
+	printf("rollnumber:");
+	scanf("%s",rolln);
+	printf("semester:");
+	scanf("%d",&sem);
+	printf("number of one credit course completed:");
+	scanf("%d",&one);
+	printf("name of one credit course completed:");
+	scanf("%s",name);
+	printf("number of value added course comleted:");
+	scanf("%d",&vadd);
+	printf("name of value added course:");
+	scanf("%s",nval);
+	printf("enter the NPTEL credit:");
+	scanf("%d",&npetel);
+
+}
