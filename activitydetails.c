e8cea904b113b8a12b1bf8c29ff2f776a8780603
santhoshkumar187 <santhoshkumars.20cse@kongu.edu>
2021-10-25 19:22:51 +0530

    activity commit

---
+++ activitydetails.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int roll , sem , pkec , prkec , pokec , prout , prise , prisewom;
+
+	printf("rollnumber:");
+	scanf("%s",roll);
+	printf("semester:");
+	scanf("%d",&sem);
+	printf("No. of papers presented inside KEC:");
+	scanf("%d",&pkec);
+	printf("No. of projects presented inside KEC:");
+	scanf("%d",&prkec);
+	printf("No. of papers presented outside KEC:");
+	scanf("%d",&pokec);
+	printf("No. of projects presented outside KEC:");
+	scanf("%d",&prout);
+	printf("No. of prize won in Coding Carnival:");
+	scanf("%d",&prise);
+	printf("No. of prize won in Hackathon:");
+	scanf("%d",&prisewon);
